91cda22803812ba34af1a42408863529c4ec4df2
YutoYasunaga <yuto.yasunaga@gmail.com>
2023-07-22 16:45:20 +0900

    Initial commit

---
+++ spec/noaccent_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Noaccent do
+  it "has a version number" do
+    expect(Noaccent::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
